fde99c127cb471ad6a34b20f4b4f76a7a5123d0b
Alex Puscas <ga27koz@mytum.de>
2024-01-30 00:00:32 +0100

    clean up matched.csv and write mapped osmnx network to gpkg
    The fmm algorithm sometimes writes "broken" linestrings as mapped detector location. Not sure why, but this should be fixed for now.
    The mapped locations are added to a osmnx map and the resulting network is written to a gpkg file. This gpkg file can be opened in qgis.

--- src/visualise_network.py
+++ src/visualise_network.py
@@ -6,13 +6,16 @@ import fiona  # this import needs to happen before osmnx
 import osmnx as ox
 import pandas as pd
 import numpy as np
+import geopandas as gpd
 
-from networkx import MultiDiGraph, Graph
+from networkx import MultiDiGraph, Graph, write_shp
 from geopandas import GeoDataFrame
 from shapely.geometry import Point
 from osmnx import settings, utils_graph, io
+from shutil import copy
 # from subprocess import Popen, PIPE
 from config import ROOT_DIR
+from add_layer import trafficRanges
 
 # Use this citation when referencing OSMnx in work
 # Boeing, G. 2017. OSMnx: New Methods for Acquiring, Constructing, Analyzing, and Visualizing Complex Street Networks.
@@ -44,7 +47,7 @@ def find_cygwin() -> str:
                     return cygname
 
 
-def save_graph_shapefile_directional(graph, filepath=None, encoding="utf-8"):
+def save_graph_shapefile_directional(graph: MultiDiGraph, filepath=None, encoding="utf-8"):
     # default filepath if none was provided
     if filepath is None:
         filepath = os.path.join(ox.settings.data_folder, "shapefile")
@@ -79,12 +82,7 @@ def get_base_graphml() -> MultiDiGraph:
     else:
         graph = ox.load_graphml(path_map_munich)
 
-    save_graph_shapefile_directional(graph, filepath=networkDataRoot)
-
-    # #graph base map
-    # _ = ox.plot_graph(graph,
-    #                   node_size=0, node_color="black",
-    #                   edge_linewidth=0.3, edge_color="white")
+    # save_graph_shapefile_directional(graph, filepath=networkDataRoot)
 
     return graph
 
@@ -105,11 +103,27 @@ def get_detectors() -> (GeoDataFrame, [Point]):
     crs = {'init': 'epsg:4326'}
     detector_gdf = GeoDataFrame(detector_df, crs=crs, geometry=geometry)
 
+    # Add the point geometry as column to points.csv for fmm in cygwin
+    #detector_df.insert(11, "geom", geometry, True)
     # TODO: replace path to local cygwin64 installation or comment it out -> contains example result of fmm
     #  in data/network/matched.csv
     detector_gdf.to_csv("D:\\cygwin64\\home\\User\\fmm\\matching\\network\\points.csv", sep=";")
 
     return detector_gdf, geometry
+
+
+def to_shp(map: MultiDiGraph, nodes: [(str, str)]):
+    # Convert nodes to a GeoDataFrame
+    node_attributes = {node: map.nodes[node] for node in map.nodes()}
+    node_coords = {node: data.get("pos", None) for node, data in map.nodes(data=True)}
+    node_geometries = [Point(coords) for coords in node_coords.values()]
+    gdf_nodes = gpd.GeoDataFrame(node_attributes.values(), geometry=node_geometries)
+
+    # Specify the output shapefile path
+    output_shapefile = networkDataRoot+"map_nodes.shp"
+
+    # Write the GeoDataFrame to a shapefile
+    gdf_nodes.to_file(output_shapefile)
 
 
 def plot():
@@ -128,31 +142,78 @@ def plot():
 
     # get mapped points
     # TODO: for the server, we can just move matched.csv to the correct location instead of reading and writing the file
-    with open("D:\\cygwin64\\home\\User\\fmm\\matching\\network\\matched.csv", mode='r') as f_cyg:
-        with open(networkDataRoot+"matched.csv", mode='w') as f_local:
-            f_local.write(f_cyg.read())
+    copy("D:\\cygwin64\\home\\User\\fmm\\matching\\network\\matched.csv",
+         networkDataRoot+"matched.csv")
 
     df_matched = pd.read_csv(networkDataRoot + "matched.csv", sep=";")
-    matched_locs = df_matched["mgeom"]
-    # add Geometry column to matched.csv so we can add it to add_layer.py
     nodes = []
+
+    # fmm seems to sometimes write out wrong information:
+    # LINESTRING(lon lat) instead of LINESTRING(lon lat,lon lat) -> fix those entries:
+    def _fix_linestring(ls: str):
+        ls_split = ls.split(',')
+        # if ls contains a valid linestring, return ls
+        if len(ls_split) == 2:
+            return ls
+        # else ls is of form LINESTRING(lon lat)
+        else:
+            lon, lat = ls[11:-1].split(' ')
+            return ls[:-1] + ",{} {})".format(lon, lat)
+
+    mgeom = df_matched["mgeom"]
+    s = pd.Series([_fix_linestring(ls) for ls in df_matched['mgeom']], name='mgeom')
+    # df_matched.loc(len(df_matched.mgeom))
+    df_matched["mgeom"] = s
+
+    matched_locs = df_matched[["id", "mgeom"]]
+
     # reformat detector locations from [LINESTRING(lon lat,lon lat)] to [(lat, lon)]
-    for node in matched_locs:
+    for id, node in matched_locs.values:
         lon_lat = node.split(',')[0][11:].split(' ')
         nodes.append((lon_lat[1], lon_lat[0]))
-    # write updated dataframe to matched.csv
+        print(node)
+        map.add_node(id, x=lon_lat[0], y=lon_lat[1])
+    # add Geometry column to matched.csv so we can add it to add_layer.py
     if 'lon' not in df_matched:
         # https://stackoverflow.com/questions/5917522/unzipping-and-the-operator#:~:text=25-,zip
         lats, lons = zip(*nodes)
         df_matched.insert(len(df_matched.columns), "lat", list(lats))
         df_matched.insert(len(df_matched.columns), "lon", list(lons))
-        df_matched.to_csv(networkDataRoot + "matched.csv", sep=";", index=False)
+    # write updated dataframe to matched.csv
+    df_matched.to_csv(networkDataRoot + "matched.csv", sep=";", index=True)
+
+    # save_graph_shapefile_directional(map, filepath=networkDataRoot)
+
+    # remove nodes that contain empty data
+    # TODO: why do some nodes contain empty data?
+    _, data = zip(*map.nodes(data=True))
+    for i, d in enumerate(data):
+        if i == 14043:
+            print(d)
+        if 'x' not in d:
+            print("at data location {} couldnt find 'x': {}".format(i,d))
+            break
 
     # add matched detector locations to base map and graph the result
-    map.add_nodes_from(nodes)
-    _ = ox.plot_graph(map, bgcolor="white",
-                      node_size=3, node_color="red",
-                      edge_linewidth=0.3, edge_color="black")
+
+
+    print(map.nodes)
+
+    # save_graph_shapefile_directional(map, filepath=networkDataRoot)
+    print("saved shapefile after adding nodes from matched.csv")
+    # digraph = map.graph
+    # to_shp(map, nodes)
+    # write_shp(map.digraph, networkDataRoot)
+    #map.add_nodes_from(nodes)
+    ox.io.save_graph_shapefile(map, networkDataRoot+"map_and_points")
+    ox.io.save_graph_geopackage(map, networkDataRoot+"map_and_points.gpkg")
+    # color edges between detector nodes
+    for u, v, k in map.edges(keys=True):
+        pass
+
+    # _ = ox.plot_graph(map, bgcolor="white",
+    #                   node_size=3, node_color="red",
+    #                   edge_linewidth=0.3, edge_color="black")
 
 
 def main():
@@ -161,3 +222,43 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # cygpath = find_cygwin()
+    #
+    # cygwin_python = "D:\\cygwin64\\bin\\bash"
+    # script_path = "D:\\cygwin64\\home\\User\\run.sh"#"D:\\cygwin64\\home\\User\\fmm\\example\\python\\fmm_test.py"
+    # my_env = os.environ.copy()
+    # my_env["PYTHONPATH"] = f"/usr/bin/python;{my_env['PYTHONPATH']}"
+    #
+    # p = subprocess.Popen(cygpath+"\\bin\\bash.exe", stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='UTF-8', env=my_env)
+    # # p.stdin.write("/bin/pwd && ")
+    # # p.stdin.write("/bin/cygpath -w /~ &&")
+    # # p.stdin.write("/bin/cd /home/User/fmm &&")
+    # # p.stdin.write()
+    # p.stdin.write("/bin/ls /home/User/fmm &&")
+    # p.stdin.write("/bin/python /home/User/fmm/example/python/fmm_example.py")
+    # p.stdin.close()
+    # out = p.stdout.read()
+    # print(out)
+
+    # try:
+    #     subprocess.run([cygwin_python, script_path], check=True)#, env=my_env)
+    # except subprocess.CalledProcessError as e:
+    #     print(f"Error executing the script: {e}")
+
+    # cygwin = subprocess.Popen(['bash'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #
+    # # args = ['D:/cygwin64/bin/run', '-p', 'D:/cygwin64/home/User/fmm/example/fmm_test.py', '-wait']
+    # # args2 = ['uname -a']
+    # # try:
+    # #     subprocess.check_output(args2, shell=True, stderr=subprocess.STDOUT)
+    # # except subprocess.CalledProcessError as e:
+    # #     raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+    #
+    # result = cygwin.communicate(input="uname -a\n".encode() +
+    #                                   # "/mnt/d/cygwin64/bin/python3.9.exe".encode() +
+    #                                   # "import os; environ = os.environ.copy()".encode() +
+    #                                   "/mnt/d/cygwin64/Cygwin.bat".encode())
+    #                                   # b"D:\cygwin64\bin/python3.9.exe D:/cygwin64/home/User/fmm/example/python/fmm_test.py")
+    # print(result[0].decode(encoding='UTF-8'))
+
+    # main()
